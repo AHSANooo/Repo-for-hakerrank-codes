d885bd37ddcb220f5c7cf339ad37df51acb1c27b
Muhammad AHSAN <ahsankhetran72@gmail.com>
2023-11-23 22:31:55 +0500

    Create Finding a number in an int array.cpp

---
+++ Finding a number in an int array.cpp
@@ -0,0 +1,54 @@
+/*Name : Muhammad Ahsan
+Roll no. : 23I-0553
+Section : E*/
+#include<iostream>
+
+using namespace std;
+int main(){
+
+const	int rows=3 , clms=2;
+	/*cout<<"Enter the number of rows : ";
+	cin>>rows;
+	cout<<"Enter the number of columns : ";
+	cin>>clms;*/
+	
+	int arr[rows][clms];
+	cout<<"Enter the elements : "<<endl;
+	
+			for (int i=0;i<rows;i++)
+		{
+			for(int j=0;j<clms;j++)
+			{
+			cin>>arr[i][j];
+			}
+		
+		}
+	
+	
+	cout<<"The matrix is : "<<endl;
+		for (int i=0;i<rows;i++)
+		{
+			for(int j=0;j<clms;j++)
+			{
+			cout<<arr[i][j]<<" ";
+			}cout<<endl;
+		
+		}cout<<endl;
+		int a;
+	cout<<"Enter the number to be found : ";
+	cin>>a;int i=0,j=0;
+				for ( i=0;i<rows;i++)
+		{
+			for( j=0;j<clms;j++)
+			{
+			if(a==arr[i][j])
+			break;
+			}
+		
+		}
+	cout<<"The value of "<<a<<" is at ("<<i<<","<<j<<")"<<endl;
+
+
+
+return 0;
+}
