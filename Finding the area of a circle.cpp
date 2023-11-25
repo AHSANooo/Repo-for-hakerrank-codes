8ffa64ecf09c37d24b04fa99d5fad30709ddd5ef
Muhammad AHSAN <ahsankhetran72@gmail.com>
2023-11-25 20:36:55 +0500

    Create Finding the area of a circle.cpp

---
+++ Finding the area of a circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+const float PI= 3.1415;
+int r;
+cout<<"MUHAMMAD AHSAN 23I-0553"<<endl;
+cout<<"Enter the radius:";
+cin>>r;
+cout<<"The diameter is:"<<r+r<<endl;
+cout<<"The circumference is:"<<2*PI*r<<endl;
+cout<<"The Area is:"<<PI*r*r<<endl;
+
+    return 0;
+}
