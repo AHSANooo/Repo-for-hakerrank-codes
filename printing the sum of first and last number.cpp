f386d14716bea27e1708cdc8d9465aec2d46c615
Muhammad AHSAN <ahsankhetran72@gmail.com>
2023-11-26 19:14:23 +0500

    Create printing the sum of first and last number.cpp

---
+++ printing the sum of first and last number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a,b,c,d;
+cout<<"MUHAMMAD AHSAN 23I-0553"<<endl;
+cout<<"Enter a four digit number:"<<endl;
+cin>>a;
+b=a/1000;
+c=a%10;
+d=b+c;
+cout<<"The sum is ="<<d<<endl;
+    return 0;
+}
