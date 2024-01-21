29b0c24868fb43855648ebf4aa7f7a1a25d27568
Muhammad AHSAN <142413686+AHSANooo@users.noreply.github.com>
2024-01-21 23:17:15 +0500

    Create getting two numbers as input and printing them.cpp

---
+++ getting two numbers as input and printing them.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main(){
+int n1,n2;
+
+	cout<<"Muhammad Ahsan 23I-0553"<<endl;
+	cout<<"Enter first number : "<<endl;
+	cin>>n1;
+	cout<<"Enter 2nd number : "<<endl;
+	cin>>n1;
+	
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
