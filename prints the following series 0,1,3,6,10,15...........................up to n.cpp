98be3af2301d44bc7def2ee0509882632add7367
Muhammad AHSAN <142413686+AHSANooo@users.noreply.github.com>
2024-01-23 20:19:04 +0500

    Create prints the following series 0,1,3,6,10,15...........................up to n.cpp

---
+++ prints the following series 0,1,3,6,10,15...........................up to n.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, m=0,i=1;
+//prints the following series 0,1,3,6,10,15...........................up to n
+	cout<<"Enter a number : ";
+	cin>>n;
+		
+		while(n>=i)
+		{
+		cout<<m<<",";
+		m+=i;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		i++;
+		}
+
+
+    return 0;
+}
+
