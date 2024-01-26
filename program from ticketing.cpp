d6baa281fc4f49f4b1488aa1a6320f3e289cfe74
Muhammad AHSAN <142413686+AHSANooo@users.noreply.github.com>
2024-01-26 09:55:00 +0500

    Create program from ticketing.cpp

---
+++ program from ticketing.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"Input:";
+cin>>a;
+if(a>=0)
+
+{
+
+if (a>=65)
+cout<<"Ticket Price : $15";
+else if (a>=13)
+cout<<"Ticket Price : $20";
+else if(a>=0)
+cout<<"Ticket Price : $15";
+
+
+
+
+
+}
+else
+cout<<"Output : Invalid input";
+
+
+return 0;
+}
